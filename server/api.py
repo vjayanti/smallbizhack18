d76bb8baaa48d9cacba7445910b618b67164f869
Vasishta Jayanti <vasishta@box.com>
2018-11-03 16:43:02 -0700

    Adding basehttphandler and managerrequestimplementer

--- server/api.py
+++ server/api.py
@@ -1,12 +1,43 @@
-import sys
-import SimpleHTTPServer
-import SocketServer
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SocketServer import ThreadingMixIn
+import json
+import os
+from manager_request_implementer import ManagerRequestImplementer
 
-PORT = 8000
+class ManagerServer(ThreadingMixIn, HTTPServer):
+    def __init__(self, address, handler):
+        HTTPServer.__init__(self, address, handler)
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+class ManagerRequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        print(self.path)
+        mri = ManagerRequestImplementer()
+        if self.path == "/cover_shifts":
+            time = self.headers.getheader('time')
+            message = mri.cover_shifts(time)
+            print(message)
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(message)
+        elif self.path == "/switch_shifts":
+            old_time = self.headers.getheader('old_time')
+            new_time = self.headers.getheader('new_time')
+            from_person = self.headers.getheader('from_person')
+            to_person = self.headers.getheader('to_person')
+            message = mri.swap_shifts(old_time, new_time, from_person, to_person)
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(message)
+        else:
+            self.send_response(404)
+
+PORT = 10000
+
+Handler = ManagerRequestHandler
+
+httpd = ManagerServer(("", PORT), Handler)
 
 print "serving at port", PORT
 httpd.serve_forever()
